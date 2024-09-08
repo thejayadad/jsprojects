05ba24daaa93d50d3a05d1b0c7d744c7d8c6200d
thejayadad <74875595+thejayadad@users.noreply.github.com>
2024-09-08 15:56:12 -0400

    jsprojects update

---
+++ jsmap/main.js
@@ -0,0 +1,6 @@
+// Declare variables
+let name = 'John';
+const age = 30;
+var city = 'New York';
+
+console.log(name, age, city);
---
+++ main.js
@@ -0,0 +1,8 @@
+
+let name = 'Jace';
+const age = 30;
+var city = 'New York';
+
+console.log("name " + name)
+console.log("age " + age)
+console.log("city " + city)
